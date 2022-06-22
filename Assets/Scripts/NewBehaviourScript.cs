7b49bb7abd17540fd90e03bcbd38ad71c2eda67b
babak_firoozi <bkfiroozi@gmail.com>
2022-06-22 18:09:18 +0430

    Photon Network StarterPack added

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Debug.Log("Log");
+        Debug.Log("Log");
+        Debug.Log("Log");
+        Debug.Log("Log");
+        
+    }
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
