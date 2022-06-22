4b8ebeda55435865707c277db45c4bbbb032957c
babak_firoozi <bkfiroozi@gmail.com>
2022-06-22 19:01:59 +0430

    ui canvas added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
-        Debug.Log("Log");
-        Debug.Log("Log");
-        Debug.Log("Log");
-        Debug.Log("Log");
-        
     }
     
     
